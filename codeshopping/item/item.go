339717d4d8348fe4f87735514e3479f38f2c456d
eleijonmarck <ericle@kth.se>
2016-11-12 15:20:47 +0100

    item of cart added to project

---
+++ codeshopping/item/item.go
@@ -0,0 +1,10 @@
+package item
+
+// Item is a item inside of a cart
+type Item struct {
+	ID         string                 `json:"id"`
+	Name       string                 `json:"name"`
+	Price      float64                `json:"price"`
+	Quantity   int                    `json:"quantity"`
+	Attributes map[string]interface{} `json:"attrs"`
+}
