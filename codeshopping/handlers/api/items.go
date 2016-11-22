eed1c38e91c179731477edfa4d3bade760d9048b
eleijonmarck <ericle@kth.se>
2016-11-22 21:47:03 +0100

    fixed the get method

--- codeshopping/handlers/api/items.go
+++ codeshopping/handlers/api/items.go
@@ -11,14 +11,19 @@ func Items(cr *cartRepository) http.Handler {
 	type ret struct {
 		Items map[string]*CartItem `json:"items"`
 	}
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if key == "" {
+			// error
+			return
+		}
+		defer r.Body.Close()
 		items, err := cr.FindAll()
 		if err != nil {
 			//error handling
 			return
 		}
-
-		w.WriteHeader(json.UnMarshal(items))
+		w.WriteHeader(http.StatusOK)
+		w.Wrire(json.UnMarshal(items))
 	})
 }
